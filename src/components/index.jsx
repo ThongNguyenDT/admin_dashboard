805107f4565051737b309d61006761cfe3225979
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-07-30 18:46:16 +0700

    build bug fix

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -10,6 +10,7 @@ export { default as Cart } from './Cart';
 export { default as Chat } from './Chat';
 // eslint-disable-next-line import/no-cycle
 export { default as Notification } from './Notification';
+// eslint-disable-next-line import/no-cycle
 export { default as UserProfile } from './UserProfile';
 export { default as SparkLine } from './Charts/SparkLine';
 export { default as LineChart } from './Charts/LineChart';
