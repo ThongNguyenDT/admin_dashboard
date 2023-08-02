8a1e2e7943a390728591cfa297f0361762775afb
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-08-02 18:47:11 +0700

    make it to admin page change path

--- src/App.js
+++ src/App.js
@@ -65,8 +65,8 @@ const App = () => {
 
               <Routes>
                 {/* dashboard  */}
-                <Route path="/" element={(<Ecommerce />)} />
-                <Route path="/ecommerce" element={(<Ecommerce />)} />
+                <Route path="/admin/" element={(<Ecommerce />)} />
+                <Route path="/admin/ecommerce" element={(<Ecommerce />)} />
 
                 {/* pages  */}
                 <Route path="/orders" element={<Orders />} />
