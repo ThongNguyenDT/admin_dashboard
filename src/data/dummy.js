093415d3e30cdaf7516722f70d8848c0f100f970
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-08-06 19:06:33 +0700

    remove dât not use

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { AiOutlineCalendar, AiOutlineShoppingCart, AiOutlineAreaChart, AiOutlineBarChart, AiOutlineStock } from 'react-icons/ai';
-import { FiShoppingBag, FiEdit, FiPieChart, FiBarChart, FiCreditCard, FiStar, FiShoppingCart } from 'react-icons/fi';
-import { BsKanban, BsBarChart, BsBoxSeam, BsCurrencyDollar, BsShield, BsChatLeft } from 'react-icons/bs';
-import { BiColorFill } from 'react-icons/bi';
 // eslint-disable-next-line no-unused-vars
 import { IoMdContacts } from 'react-icons/io';
 import { RiContactsLine, RiStockLine } from 'react-icons/ri';
