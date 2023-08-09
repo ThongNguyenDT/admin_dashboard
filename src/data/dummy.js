502a008d8335f3a630024d0cb3b69ef4ebb2ccc7
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-08-09 20:16:10 +0700

    i dont net chart review

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -507,42 +507,6 @@ export const links = [
         name: 'kanban',
         icon: <BsKanban />,
       },
-  {
-    title: 'Charts',
-    links: [
-      {
-        name: 'line',
-        icon: <AiOutlineStock />,
-      },
-      {
-        name: 'area',
-        icon: <AiOutlineAreaChart />,
-      },
-
-      {
-        name: 'bar',
-        icon: <AiOutlineBarChart />,
-      },
-      {
-        name: 'pie',
-        icon: <FiPieChart />,
-      },
-      {
-        name: 'financial',
-        icon: <RiStockLine />,
-      },
-      {
-        name: 'color-mapping',
-        icon: <BsBarChart />,
-      },
-      {
-        name: 'pyramid',
-        icon: <GiLouvrePyramid />,
-      },
-      {
-        name: 'stacked',
-        icon: <AiOutlineBarChart />,
-      },
     ],
   },
 ];
