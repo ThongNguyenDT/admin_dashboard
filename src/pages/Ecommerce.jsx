6643849e61a58529ab42bd4828140fc664e854e7
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-08-20 18:51:39 +0700

    remove don use

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BsCurrencyDollar } from 'react-icons/bs';
 import { GoPrimitiveDot } from 'react-icons/go';
 import { IoIosMore } from 'react-icons/io';
-import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
-import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { Stacked, Pie, Button, SparkLine } from '../components';
+import { earningData, medicalproBranding, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 // import product9 from '../data/product9.jpg';
 
